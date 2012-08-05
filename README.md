3c717b80af84bce17bb7b5bd986108a7a010c60a
Ben Plum <benjaminplum@gmail.com>
2012-08-05 19:55:50 -0400

    Commented source
    Alphabatized default options; Added link to
    documentation and examples

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Macaroon
 ========
 
 A jQuery plugin for simple access to browser cookies.
+
+[Documentation and Examples](http://www.benjaminplum.com/projects/macaroon/)
--- jquery.bp.macaroon.js
+++ jquery.bp.macaroon.js
@@ -1,19 +1,25 @@
 /*
  * Macaroon Plugin - Simple cookie access
  * @author Ben Plum <benjaminplum@gmail.com>
- * @version 1.0.1
+ * @version 1.0.2
  *
  * Copyright © 2012 Ben Plum <ben@benjaminplum.com>
  * Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
  */
  
 (function($) {
+	
+	// Default Options
 	var options = {
+		domain: null,
 		expires: 7,
-		path: "/",
-		domain: null
+		path: "/"
 	};
+	
+	// Methods
 	var methods = {
+		
+		// Create or update a cooke
 		create: function(key, value, options) {
 			var date = new Date();
 			date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
@@ -22,6 +28,8 @@
 			var domain = (options.domain != null) ? "; domain=" + options.domain : "";
 			document.cookie = key + "=" + value + expires + domain + path;
 		},
+		
+		// Read a cookie
 		read: function(key) {
 			var keyString = key + "=";
 			var cookieArray = document.cookie.split(';');
@@ -34,19 +42,26 @@
 			}
 			return null;
 		},
+		
+		// Erase a cookie
 		erase: function(key) {
 			methods.create(key, "", { expires: -1 });
 		}
 	};
 	
+	
+	// Define Plugin 
 	$.macaroon = function(key, value, opts) {
+		// Set defaults
 		if (typeof key == "object") {
 			options = jQuery.extend(options, key);
 			return null;
 		} else {
+			// Override defaults
 			opts = jQuery.extend(options, opts);
 		}
 		
+		// Delegate intent
 		if (typeof key != "undefined") {
 			if (typeof value != "undefined") {
 				if (value == null) {
