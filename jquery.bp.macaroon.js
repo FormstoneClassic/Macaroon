ec88964bd72c19cdc32b75873d772cd64fd4fd20
Ben Plum <mr@benplum.com>
2012-09-22 23:22:25 -0400

    Refactored for performance and smaller file size; Namspaced individual body-bound events for each instance;

--- jquery.bp.macaroon.js
+++ jquery.bp.macaroon.js
@@ -1,13 +1,13 @@
 /*
  * Macaroon Plugin - Simple cookie access
  * @author Ben Plum
- * @version 1.1
+ * @version 1.2
  *
  * Copyright © 2012 Ben Plum <mr@benplum.com>
  * Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
  */
  
-(function($) {
+if (jQuery) (function($) {
 	
 	// Default Options
 	var options = {
@@ -16,39 +16,37 @@
 		path: "/"
 	};
 	
-	// Methods
-	var methods = {
+	// Public Methods
+	var pub = {};
 		
-		// Create or update a cooke
-		_create: function(key, value, options) {
-			var date = new Date();
-			date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
-			var expires = "; expires=" + date.toGMTString();
-			var path = "; path=" + options.path
-			var domain = (options.domain != null) ? "; domain=" + options.domain : "";
-			document.cookie = key + "=" + value + expires + domain + path;
-		},
-		
-		// Read a cookie
-		_read: function(key) {
-			var keyString = key + "=";
-			var cookieArray = document.cookie.split(';');
-			for(var i = 0; i < cookieArray.length; i++) {
-				var cookie = cookieArray[i];
-				while (cookie.charAt(0) == ' ') {
-					cookie = cookie.substring(1, cookie.length);
-				}
-				if (cookie.indexOf(keyString) == 0) return cookie.substring(keyString.length, cookie.length);
-			}
-			return null;
-		},
-		
-		// Erase a cookie
-		_erase: function(key) {
-			methods._create(key, "", $.extend({}, options, { expires: -1 }));
-		}
-	};
+	// Create or update a cooke
+	function _create(key, value, options) {
+		var date = new Date();
+		date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+		var expires = "; expires=" + date.toGMTString();
+		var path = "; path=" + options.path
+		var domain = (options.domain != null) ? "; domain=" + options.domain : "";
+		document.cookie = key + "=" + value + expires + domain + path;
+	}
 	
+	// Read a cookie
+	function _read(key) {
+		var keyString = key + "=";
+		var cookieArray = document.cookie.split(';');
+		for(var i = 0; i < cookieArray.length; i++) {
+			var cookie = cookieArray[i];
+			while (cookie.charAt(0) == ' ') {
+				cookie = cookie.substring(1, cookie.length);
+			}
+			if (cookie.indexOf(keyString) == 0) return cookie.substring(keyString.length, cookie.length);
+		}
+		return null;
+	}
+	
+	// Erase a cookie
+	function _erase(key) {
+		_create(key, "", $.extend({}, options, { expires: -1 }));
+	}
 	
 	// Define Plugin 
 	$.macaroon = function(key, value, opts) {
@@ -64,14 +62,13 @@
 		// Delegate intent
 		if (typeof key != "undefined") {
 			if (typeof value != "undefined") {
-				console.log(value);
 				if (value == null) {
-					methods._erase(key);
+					_erase(key);
 				} else {
-					methods._create(key, value, opts);
+					_create(key, value, opts);
 				}
 			} else {
-				return methods._read(key);
+				return _read(key);
 			}
 		}
 	};
