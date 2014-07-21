0212f3260f5d8f095c3d5d6958644a1d60add962
Ben Plum <mr@benplum.com>
2014-07-21 10:42:12 -0400

    Fixing bug

--- src/jquery.fs.macaroon.js
+++ src/jquery.fs.macaroon.js
@@ -26,11 +26,11 @@
 		var date = new Date();
 
 		opts = $.extend({}, options, opts);
-		if (opts.expires) {
+		if (opts.expires || typeof opts.expires === "number") {
 			date.setTime(date.getTime() + opts.expires);
 		}
 
-		var expires = (opts.expires && typeof opts.expires === "number") ? "; expires=" + date.toGMTString() : "",
+		var expires = (opts.expires || typeof opts.expires === "number") ? "; expires=" + date.toGMTString() : "",
 			path = (opts.path) ? "; path=" + opts.path : "",
 			domain = (opts.domain) ? "; domain=" + opts.domain : "";
 
